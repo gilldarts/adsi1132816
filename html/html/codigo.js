2a41b4eb7e2c26bd9d1c352fc4c87b089d950230
gilldarts <jaime.palomino97@hotmail.com>
2016-06-02 20:27:48 -0500

    agregar archivo css a adsi-git

---
+++ html/html/codigo.js
@@ -0,0 +1,8 @@
+var nombre = prompt('cual es su nombre?');
+
+if (nombre == 'gilldarts') {
+	alert('bienvenido: '+nombre+' el maestro del gremio')
+}
+else{
+	alert('bienvenido: '+nombre+' rango S')
+}
